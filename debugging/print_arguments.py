28d19b7efc9ee974e8b75bc92ffee4b052fe1ddd
gastii11 <10632@holbertonstudents.com>
2025-05-05 14:19:21 +0000

    use enumerate(sys.argv) lo que permite incluir sys.argv[0] en la impresion

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-# Verificamos si hay argumentos
 if len(sys.argv) > 1:
     for i, arg in enumerate(sys.argv):
-        print(f"Argumento {i}: {arg}")
+        print(f"{arg}")
 else:
     print("No se proporcionaron argumentos.")
